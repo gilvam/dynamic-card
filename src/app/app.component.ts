634709489b126570bd51e78483b4735f94b31f85
Gilvam Mourao <gilvamm@gmail.com>
2024-08-17 22:47:43 -0300

    feat: version config 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardSimpleComponent } from './card/card-simple/card-simple.component';
-import { CardDoubleComponent } from './card/card-double/card-double.component';
-import { CardTableComponent } from './card/card-table/card-table.component';
 import { CardDisplayComponent } from './card-display/card-display.component';
 
 @Component({
@@ -13,30 +10,68 @@ import { CardDisplayComponent } from './card-display/card-display.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  list = [
+  list2 = [
     {
       name: 'Users',
       cards: [
-        { name: 'Card 1', component: CardSimpleComponent, style: { gridArea: '1 / 1 / 1 / 1', color: '#5585b5', height: '200px' } },
-        { name: 'Card 2', component: CardDoubleComponent, style: { gridArea: '1 / 2 / 1 / 2', color: '#53a8b6', height: '200px' } },
-        { name: 'Card 3', component: CardTableComponent, style: { gridArea: '1 / 3 / 1 / 3', color: '#79c2d0', height: '200px' } },
+        { name: 'Card 1', component: 'app-card-simple', style: { gridArea: '1 / 1 / 1 / 1', color: '#5585b5', height: '150px' } },
+        { name: 'Card 2', component: 'app-card-double', style: { gridArea: '1 / 2 / 1 / 2', color: '#53a8b6', height: '150px' } },
+        { name: 'Card 3', component: 'app-card-table', style: { gridArea: '1 / 3 / 1 / 3', color: '#79c2d0', height: '150px' } },
       ]
     },
     {
       name: 'Oriented',
       cards: [
-        { name: 'Card 1', component: CardSimpleComponent, style: { gridArea: '1 / 1 / 1 / 1', color: '#eea29a', height: '200px' } },
+        { name: 'Card 1', component: 'app-card-double', style: { gridArea: '1 / 1 / 1 / 1', color: '#eea29a', height: '200px' } },
       ]
     },
     {
       name: 'Previously directed',
       cards: [
-        { name: 'Card 1', component: CardSimpleComponent, style: { gridArea: '1 / 1 / 1 / 4', color: '#b9936c', height: '' } },
-        { name: 'Card 2', component: CardSimpleComponent, style: { gridArea: '1 / 4 / 1 / 7', color: '#dac292', height: '' } },
-        { name: 'Card 3', component: CardSimpleComponent, style: { gridArea: '2 / 1 / 2 / 4', color: '#e6e2d3', height: '' } },
-        { name: 'Card 4', component: CardSimpleComponent, style: { gridArea: '2 / 4 / 2 / 7', color: '#c4b7a6', height: '' } },
-        { name: 'Card 5', component: CardTableComponent, style: { gridArea: '1 / 7 / 3 / 9', color: '#b1cbbb', height: '500px' } },
+        { name: 'Card 1', component: 'app-card-simple', style: { gridArea: '1 / 1 / 1 / 4', color: '#b9936c', height: '' } },
+        { name: 'Card 2', component: 'app-card-simple', style: { gridArea: '1 / 4 / 1 / 7', color: '#dac292', height: '' } },
+        { name: 'Card 3', component: 'app-card-simple', style: { gridArea: '2 / 1 / 2 / 4', color: '#e6e2d3', height: '' } },
+        { name: 'Card 4', component: 'app-card-simple', style: { gridArea: '2 / 4 / 2 / 7', color: '#c4b7a6', height: '' } },
+        { name: 'Card 5', component: 'app-card-table', style: { gridArea: '1 / 7 / 3 / 9', color: '#b1cbbb', height: '500px' } },
       ]
     }
   ];
+
+  list = [
+    {
+      name: 'Users',
+      gridTemplateArea: ['A B C'],
+      cards: [
+        { id: 'A', name: 'Card 1', component: 'app-card-simple', style: { color: '#5585b5', height: '150px' } },
+        { id: 'B', name: 'Card 2', component: 'app-card-double', style: { color: '#53a8b6', height: '150px' } },
+        { id: 'C', name: 'Card 3', component: 'app-card-table', style: { color: '#79c2d0', height: '150px' } },
+      ]
+    },
+    {
+      name: 'Oriented',
+      gridTemplateArea: ['A'],
+      cards: [
+        { id: 'A', name: 'Card 1', component: 'app-card-double', style: { color: '#eea29a', height: '200px' } },
+      ]
+    },
+    {
+      name: 'Previously directed',
+      gridTemplateArea: [
+        'A A A B B B E E',
+        'C C C D D D E E'
+      ],
+      cards: [
+        { id: 'A', name: 'Card 1', component: 'app-card-simple', style: { color: '#9CA986', height: '' } },
+        { id: 'B', name: 'Card 2', component: 'app-card-simple', style: { color: '#96C291', height: '' } },
+        { id: 'C', name: 'Card 3', component: 'app-card-simple', style: { color: '#5B9A8B', height: '' } },
+        { id: 'D', name: 'Card 4', component: 'app-card-simple', style: { color: '#81A263', height: '' } },
+        { id: 'E', name: 'Card 5', component: 'app-card-table', style: { color: '#73BBA3', height: '500px' } },
+      ]
+    }
+  ];
+
+  getGridTemplateArea(area: string[]): string {
+    return area.map(row => `"${row}"`).join(' ');
+  }
+
 }
--- src/app/card-display/card-display.component.ts
+++ src/app/card-display/card-display.component.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component, ComponentFactoryResolver, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import { CardSimpleComponent } from '../card/card-simple/card-simple.component';
+import { CardDoubleComponent } from '../card/card-double/card-double.component';
+import { CardTableComponent } from '../card/card-table/card-table.component';
 
 @Component({
   selector: 'app-card-display',
@@ -12,6 +15,12 @@ export class CardDisplayComponent implements AfterViewInit {
 
   @Input() card!: { name: string, component: any, style: any };
 
+  private componentsMap: { [key: string]: any } = {
+    'app-card-simple': CardSimpleComponent,
+    'app-card-double': CardDoubleComponent,
+    'app-card-table': CardTableComponent,
+  };
+
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
@@ -20,14 +29,16 @@ export class CardDisplayComponent implements AfterViewInit {
   }
 
   renderCards() {
-    if (!this.cardContainer && !this.card) {
-      return;
+    const component = this.componentsMap[this.card.component];
+
+    if(!component) {
+      throw new Error(`Component ${this.card.component} not found`);
     }
 
     this.cardContainer.clear();
 
-    const factory = this.componentFactoryResolver.resolveComponentFactory(this.card.component);
+    const factory = this.componentFactoryResolver.resolveComponentFactory(component);
     const componentRef = this.cardContainer.createComponent(factory);
-    // Object.assign(componentRef.location.nativeElement.style, this.card.style);
+    Object.assign(componentRef.location.nativeElement.style, { background: this.card.style.color });
   }
 }
