59480520e073b4c3baa87e39f2a6a15a7d0ecf32
Gilvam Mourao <gilvamm@gmail.com>
2025-02-22 12:24:17 -0300

    chore: add SWC test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,22 @@
+const { swcAngularJestTransformer } = require("@jscutlery/swc-angular-preset");
+
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  transform: {
+    "^.+\\.(ts|mjs|js)$": swcAngularJestTransformer(),
+    "^.+\\.(html)$": [
+      "jest-preset-angular",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.(html|svg)$",
+      },
+    ],
+  },
+  snapshotSerializers: [
+    "jest-preset-angular/build/serializers/no-ng-attributes",
+    "jest-preset-angular/build/serializers/ng-snapshot",
+    "jest-preset-angular/build/serializers/html-comment",
+  ],
+  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
 };
